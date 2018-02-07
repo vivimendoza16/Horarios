36f79a413d18e1bc0133b4be0c4cdc7838b9563d
VivianM <vc.mendozab@uniandes.edu.co>
2018-02-06 23:11:56 -0500

    Commit Horarios

---
+++ scripts/app.js
@@ -0,0 +1,228 @@
+
+(function () {
+    'use strict';
+
+    var app = {
+        isLoading: true,
+        visibleCards: {},
+        selectedTimetables: [],
+        spinner: document.querySelector('.loader'),
+        cardTemplate: document.querySelector('.cardTemplate'),
+        container: document.querySelector('.main'),
+        addDialog: document.querySelector('.dialog-container')
+    };
+
+
+    /*****************************************************************************
+     *
+     * Event listeners for UI elements
+     *
+     ****************************************************************************/
+
+    document.getElementById('butRefresh').addEventListener('click', function () {
+        // Refresh all of the metro stations
+        app.updateSchedules();
+    });
+
+    document.getElementById('butAdd').addEventListener('click', function () {
+        // Open/show the add new station dialog
+        app.toggleAddDialog(true);
+    });
+
+    document.getElementById('butAddCity').addEventListener('click', function () {
+
+
+        var select = document.getElementById('selectTimetableToAdd');
+        var selected = select.options[select.selectedIndex];
+        var key = selected.value;
+        var label = selected.textContent;
+        if (!app.selectedTimetables) {
+            app.selectedTimetables = [];
+        }
+        app.getSchedule(key, label);
+        app.selectedTimetables.push({key: key, label: label});
+        app.saveselectedTimetables();
+        app.toggleAddDialog(false);
+    });
+
+    document.getElementById('butAddCancel').addEventListener('click', function () {
+        // Close the add new station dialog
+        app.toggleAddDialog(false);
+    });
+
+
+    /*****************************************************************************
+     *
+     * Methods to update/refresh the UI
+     *
+     ****************************************************************************/
+
+    // Toggles the visibility of the add new station dialog.
+    app.toggleAddDialog = function (visible) {
+        if (visible) {
+            app.addDialog.classList.add('dialog-container--visible');
+        } else {
+            app.addDialog.classList.remove('dialog-container--visible');
+        }
+    };
+
+    // Updates a timestation card with the latest weather forecast. If the card
+    // doesn't already exist, it's cloned from the template.
+
+    app.updateTimetableCard = function (data) {
+        var key = data.key;
+        var dataLastUpdated = new Date(data.created);
+        var schedules = data.schedules;
+        var card = app.visibleCards[key];
+
+        if (!card) {
+            var label = data.label.split(', ');
+            var title = label[0];
+            var subtitle = label[1];
+            card = app.cardTemplate.cloneNode(true);
+            card.classList.remove('cardTemplate');
+            card.querySelector('.label').textContent = title;
+            card.querySelector('.subtitle').textContent = subtitle;
+            card.removeAttribute('hidden');
+            app.container.appendChild(card);
+            app.visibleCards[key] = card;
+        }
+        card.querySelector('.card-last-updated').textContent = data.created;
+
+        var scheduleUIs = card.querySelectorAll('.schedule');
+        for(var i = 0; i<4; i++) {
+            var schedule = schedules[i];
+            var scheduleUI = scheduleUIs[i];
+            if(schedule && scheduleUI) {
+                scheduleUI.querySelector('.message').textContent = schedule.message;
+            }
+        }
+
+        if (app.isLoading) {
+            app.spinner.setAttribute('hidden', true);
+            app.container.removeAttribute('hidden');
+            app.isLoading = false;
+        }
+    };
+
+    /*****************************************************************************
+     *
+     * Methods for dealing with the model
+     *
+     ****************************************************************************/
+
+
+    app.getSchedule = function (key, label) {
+        var url = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/' + key;
+
+        if ('caches' in window) {
+
+       caches.match(url).then(function(response) {
+         if (response) {
+           response.json().then(function updateFromCache(json) {
+             var results = json.query.results;
+             results.key = key;
+             results.label = label;
+             results.created = json.query.created;
+             app.updateTimetableCard(results);
+           });
+         }
+       });
+     }
+
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    var response = JSON.parse(request.response);
+                    var result = {};
+                    result.key = key;
+                    result.label = label;
+                    result.created = response._metadata.date;
+                    result.schedules = response.result.schedules;
+                    app.updateTimetableCard(result);
+                }
+            } else {
+                // Return the initial weather forecast since no data is available.
+                app.updateTimetableCard(initialStationTimetable);
+            }
+        };
+        request.open('GET', url);
+        request.send();
+    };
+
+    // Iterate all of the cards and attempt to get the latest timetable data
+    app.updateSchedules = function () {
+        var keys = Object.keys(app.visibleCards);
+        keys.forEach(function (key) {
+            app.getSchedule(key);
+        });
+    };
+
+    app.saveselectedTimetables = function() {
+      var selectedTimetables= JSON.stringify(app.selectedTimetables);
+      localStorage.selectedTimetables = selectedTimetables;
+    };
+    /*
+     * Fake timetable data that is presented when the user first uses the app,
+     * or when the user has not saved any stations. See startup code for more
+     * discussion.
+     */
+
+    var initialStationTimetable = {
+
+        key: 'metros/1/bastille/A',
+        label: 'Bastille, Direction La Défense',
+        created: '2017-07-18T17:08:42+02:00',
+        schedules: [
+            {
+                message: '0 mn'
+            },
+            {
+                message: '2 mn'
+            },
+            {
+                message: '5 mn'
+            }
+        ]
+
+
+    };
+
+
+    /************************************************************************
+     *
+     * Code required to start the app
+     *
+     *
+     ************************************************************************/
+//    app.selectedTimetables = localStorage.selectedTimetables;
+//    if (app.selectedTimetables) {
+//    app.selectedTimetables = JSON.parse(app.selectedTimetables);
+//    app.selectedTimetables.forEach(function() {
+//    app.getSchedule();
+//   });
+// } else {
+
+//   app.updateSchedules(initialStationTimetable);
+//   app.selectedTimetables = [
+//     {key: initialStationTimetable.key, label: initialStationTimetable.label}
+//   ];
+//   app.selectedTimetables();
+ //}
+
+
+
+    app.getSchedule('metros/1/bastille/A', 'Bastille, Direction La Défense');
+    app.selectedTimetables = [
+        {key: initialStationTimetable.key, label: initialStationTimetable.label}
+    ];
+
+    if ('serviceWorker' in navigator) {
+       navigator.serviceWorker
+                .register('./service-worker.js')
+                .then(function() { console.log('Service Worker Registered'); });
+     }
+
+
+})();
---
+++ service-worker.js
@@ -0,0 +1,58 @@
+var dataCacheName = 'SchedulesData1weatherData-v1';
+var cacheName = 'Train-Lines+1';
+var filesToCache = [
+  '/',
+  '/manifest.json',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
